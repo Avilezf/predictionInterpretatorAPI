154123b0fb655e723ebc19fd0c3c24d38a1bad89
Luis Llanos <avilezf@uninorte.edu.co>
2022-11-20 16:52:52 -0500

    Test and Optimizations to the code

---
+++ src/test/java/com/example/javapythoninterpreter/service/impl/PredictionServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.example.javapythoninterpreter.service.impl;
+
+import com.example.javapythoninterpreter.service.PredictionService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PredictionServiceImplTest {
+
+    @Autowired
+    PredictionService predictionService;
+
+    @Test
+    void getPrediction() {
+        String prediction = this.predictionService.getPrediction(13, 212111);
+        assertEquals("[0.90410959]", prediction.trim());
+    }
+
+}
